db6dbbd1164d6d20341fa885a82acafd45b46df6
Leander Bindt <leander.bindt@ziggo.nl>
2021-09-08 19:38:13 +0200

    Update 1.1.js
    De codering van 1.1 is netter gemaakt en de robot heeft nu een hoed
    
    Co-Authored-By: Lotte004 <90279139+Lotte004@users.noreply.github.com>

--- src/js/chapter-1/1.1.js
+++ src/js/chapter-1/1.1.js
@@ -5,10 +5,19 @@ function setup() {
 }
 
 function draw () {
-    rect(500, 200, 100, 100);
+    // De robot
+    fill('white');
     rect(200, 300, 700, 600);
     rect(400, 700, 300, 100);
 
     circle(400, 500, 200);
     circle(700, 500, 200);
+
+    // De hoed
+    fill('brown');
+    rect(500, 200, 100, 100);
+    rect(150, 250, 800, 50);
+
+    // Het symbool
+    //...
 }
