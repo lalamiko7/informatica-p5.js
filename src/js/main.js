c4dfc260bc3c358d543703e9f1168bb7d10267b2
Leander Bindt <leander.bindt@ziggo.nl>
2021-09-13 16:42:15 +0200

    Update website
    De navigatiebalk werkt bijna. De slide in en out animaties zijn bijna af

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
-function nav_menu_animatie(item) {
-    item.classList.toggle("nav-menu-animatie");
+var nav_name_count = 0;
+var nav_links_count = 0;
+
+function nav_menu_animatie(nav_menu_symbool) {
+    nav_menu_symbool.classList.toggle("nav-menu-symbool-animatie");
+
+    var nav_name = document.getElementById("nav-name");
+    if (nav_name_count == 0) {
+        nav_name.classList.toggle("nav-name-animatie-out");
+        nav_name_count += 1;
+    } else if (nav_name_count == 1) {
+        nav_name.classList.toggle("nav-name-animatie-out");
+        nav_name.classList.toggle("nav-name-animatie-in");
+        nav_name_count += 1;
+    } else if (nav_name_count == 2) {
+        nav_name.classList.toggle("nav-name-animatie-in");
+        nav_name.classList.toggle("nav-name-animatie-out");
+        nav_name_count -= 1;
+    }
+
+    var nav_links = document.getElementById("nav-links");
+    if (nav_links_count == 0) {
+        nav_links.classList.toggle("nav-links-animatie-in");
+        nav_links_count += 1;
+    } else if (nav_links_count == 1) {
+        nav_links.classList.toggle("nav-links-animatie-in");
+        nav_links.classList.toggle("nav-links-animatie-out");
+        nav_links_count += 1;
+    } else if (nav_links_count == 2) {
+        nav_links.classList.toggle("nav-links-animatie-out");
+        nav_links.classList.toggle("nav-links-animatie-in");
+        nav_links_count -= 1;
+    }
 }
