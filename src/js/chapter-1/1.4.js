4846241b77c6dd203c192546c9066a42917fa536
Lotte004 <lotte.mulder12@gmail.com>
2021-09-18 22:10:08 +0200

    Update 1.4.js
    Opdracht afgemaakt en leuke details toegevoegd. :)
    
    Co-Authored-By: Leander Bindt <48926706+lalamiko7@users.noreply.github.com>

--- src/js/chapter-1/1.4.js
+++ src/js/chapter-1/1.4.js
@@ -4,17 +4,69 @@ function setup() {
 }
 
 function draw() {
-    // Quadrilateral
-    //quad(100, 100, 300, 40, 400, 110, 100, 110);
-
-    // Polygon
+    
+   fill('white');
+   strokeWeight(1);
+   triangle(50, 20, 60, 30, 70, 20);
+   triangle(50, 40, 60, 30, 70, 40);
+   triangle(50, 40, 60, 50, 70, 40);
+   triangle(50, 60, 60, 50, 70, 60);
+   triangle(50, 60, 60, 70, 70, 60);
+   triangle(50, 80, 60, 70, 70, 80);
+   triangle(50, 80, 60, 80, 70, 80);
+   triangle(50, 80, 60, 90, 70, 80);
+   triangle(50, 100, 60, 90, 70, 100);
+    
+   quad(100, 80, 110, 40, 130, 40, 140, 80);
+    
     beginShape();
-    vertex(10, 10);
-    vertex(200, 15);
-    vertex(300, 20);
-    vertex(100, 50);
-    vertex(420, 70);
-    vertex(69, 69);
+    vertex(165, 40);
+    vertex(170, 55);
+    vertex(180, 60);
+    vertex(170, 65);
+    vertex(175, 80);
+    vertex(165, 70);
+    vertex(155, 80);
+    vertex(160, 65);
+    vertex(150, 60);
+    vertex(160, 55);
     endShape(CLOSE);
+    
+    beginShape();
+    vertex(220, 40);
+    vertex(250, 55);
+    vertex(240, 55);
+    vertex(240, 80);
+    vertex(230, 80);
+    vertex(230, 70);
+    vertex(210, 70);
+    vertex(210, 80);
+    vertex(200, 80);
+    vertex(200, 55);
+    vertex(190, 55);
+    endShape(CLOSE);
+    
+   // snap het niet help, point(295,45);
+   // snap het niet help, point(335, 45);
+    arc(295, 60, 30, 30, 0, TWO_PI-QUARTER_PI, PIE);
+    arc(335, 60, 30, 30, 230, PI, PIE);
+    
+    beginShape();
+    vertex(400, 80);
+    bezierVertex(460, 40, 400, 40, 400, 60);
+    bezierVertex(400, 40, 340, 40, 400, 80);
+    endShape();
+    
+    ellipse(475, 60, 50, 30);
+    
+   triangle(530, 20, 540, 30, 550, 20);
+   triangle(530, 40, 540, 30, 550, 40);
+   triangle(530, 40, 540, 50, 550, 40);
+   triangle(530, 60, 540, 50, 550, 60);
+   triangle(530, 60, 540, 70, 550, 60);
+   triangle(530, 80, 540, 70, 550, 80);
+   triangle(530, 80, 540, 80, 550, 80);
+   triangle(530, 80, 540, 90, 550, 80);
+   triangle(530, 100, 540, 90, 550, 100);
 
 }
