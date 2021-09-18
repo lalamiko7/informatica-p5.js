d5a9e239e3080507cb64ab79509ba0dd80157fb7
Lotte004 <lotte.mulder12@gmail.com>
2021-09-18 22:41:44 +0200

    Update 1.5.js
    Geprobeerd met bezierVertex de outline te maken. Het werkte echter niet, de begin shape is daardoor overgebleven...
    
    Co-Authored-By: Leander Bindt <48926706+lalamiko7@users.noreply.github.com>

--- src/js/chapter-1/1.5.js
+++ src/js/chapter-1/1.5.js
@@ -1,26 +1,13 @@
-function preload() {
-    img = loadImage('https://test.leanderbindt.dev/world.svg');
-}
-
 function setup() {
     createCanvas(1425, 770);
+    background('lightgrey');
 }
 
 function draw() {
-    background(100);
-
-    stroke("#2977E2");
-    strokeWeight(7);
-
-    fill("#29ABE2");
-    circle(1425 / 2, 770 / 2, 400);
-
-    strokeWeight(3);
-    fill("#00B245");
     
-    image(world, 1425 / 2, 770 / 2);
-
-    noStroke();
-    fill("black");
-    text(mouseX + ", " + mouseY, 1425 / 2, 770 / 2);
+   fill('#5d9af5');
+   stroke(73, 135, 227);
+   strokeWeight(5);
+   circle(1425/2, 770/2, 400, 400);
+    
 }
