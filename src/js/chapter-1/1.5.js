c64133342623d384eae853a3d4e269101bf7254f
Leander Bindt <leander.bindt@ziggo.nl>
2021-09-20 15:41:10 +0200

    Update les 1.5 hoofdstuk 1
    Rotatie toegevoegd aan 1.5 en bezig met de footer op de index pagina

--- src/js/chapter-1/1.5.js
+++ src/js/chapter-1/1.5.js
@@ -1,29 +1,36 @@
+let r;
+
 function setup() {
     createCanvas(1425, 770);
+    r = 0;
 }
 
 function draw() {
     background('lightgrey');
+
+    translate(width / 2, height / 2);
+    rotate(r * PI);
+    r += 0.01;
 
     const transparent = color(0);
     transparent.setAlpha(0);
     fill(transparent);
     stroke('#5d8d9f');
     strokeWeight(10);
-    circle(1425 / 2, 770 / 2, 400);
+    circle(0, 0, 400);
 
     noStroke();
     fill('#5d8d9f');
-    circle(1425 / 2, 770 / 2, 400, 400);
+    circle(0, 0, 400);
 
     fill('#ffa3b9');
-    arc(1425 / 2, 770 / 2, 400, 400, HALF_PI, 0 - HALF_PI);
-    circle(1425 / 2, 770 / 2 - 100, 200);
+    arc(0, 0, 400, 400, HALF_PI, 0 - HALF_PI);
+    circle(0, 0 - 100, 200);
 
     fill('#5d8d9f');
-    circle(1425 / 2, 770 / 2 + 100, 200);
-    circle(1425 / 2, 770 / 2 - 100, 50);
+    circle(0, 0 + 100, 200);
+    circle(0, 0 - 100, 50);
     
     fill('#ffa3b9');
-    circle(1425 / 2, 770 / 2 + 100, 50);
+    circle(0, 0 + 100, 50);
 }
