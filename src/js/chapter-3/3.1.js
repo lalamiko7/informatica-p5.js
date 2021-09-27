fba721a169a54cf8608a0ae224095e51ff6b0116
Lotte004 <lotte.mulder12@gmail.com>
2021-09-27 15:28:56 +0200

    Opdracht 3.1
    Start gemaakt met 3.1
    
    Co-Authored-By: Leander Bindt <48926706+lalamiko7@users.noreply.github.com>

---
+++ src/js/chapter-3/3.1.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(1425, 770);
+    background('lightgrey');
+}
+
+function draw() {
+  strokeWeight(10);
+  
+  ellipse(50,50,50);
+  ellipse(125,50,50);
+  ellipse(200,50,50);
+  ellipse(275,50,50);
+  ellipse(350,50,50);
+    
+  ellipse(50,125,50);
+  ellipse(125,125,50);
+  ellipse(200,125,50);
+  ellipse(275,125,50);
+  ellipse(350,125,50);
+}
