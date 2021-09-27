2c7380d5b14bd4ecfbc9d0e9175b0347b25ed89d
Lotte004 <lotte.mulder12@gmail.com>
2021-09-27 15:48:42 +0200

    Update 3.1.js

--- src/js/chapter-3/3.1.js
+++ src/js/chapter-3/3.1.js
@@ -1,20 +1,37 @@
+var x;
+var y;
+var spacing;
+var circles;
+var size;
+
 function setup() {
-    createCanvas(1425, 770);
+    x = 1425;
+    y = 770;
+    createCanvas(x, y);
     background('lightgrey');
+    
+    spacing = 25;
+    circles = 6;
+    size = 50;
 }
 
 function draw() {
-  strokeWeight(10);
-  
-  ellipse(50,50,50);
-  ellipse(125,50,50);
-  ellipse(200,50,50);
-  ellipse(275,50,50);
-  ellipse(350,50,50);
+    translate(x / 2, y / 2);
     
-  ellipse(50,125,50);
-  ellipse(125,125,50);
-  ellipse(200,125,50);
-  ellipse(275,125,50);
-  ellipse(350,125,50);
+    strokeWeight(10);
+
+    for (i = 0; i < circles; i++) {
+        Console.log('test');
+    }
+    ellipse(50,50,50);
+    ellipse(125,50,50);
+    ellipse(200,50,50);
+    ellipse(275,50,50);
+    ellipse(350,50,50);
+
+    ellipse(50,125,50);
+    ellipse(125,125,50);
+    ellipse(200,125,50);
+    ellipse(275,125,50);
+    ellipse(350,125,50);
 }
