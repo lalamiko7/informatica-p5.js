a83a7b3c92752bcfca1852a7412e5368bd04e7d9
Leander Bindt <leander.bindt@ziggo.nl>
2021-10-13 17:14:32 +0200

    Update eindopdracht.js

--- src/js/chapter-1/eindopdracht.js
+++ src/js/chapter-1/eindopdracht.js
@@ -1,9 +1,25 @@
+// Definieer variabel voor de grote van album
+let album_size;
+let seed;
+
 function setup() {
     createCanvas(1425, 770);
+
+    // Geef variabel een waarde
+    album_size = 600;
 }
 
 function draw() {
-    background('lightgrey');
+    // Maak nulpunt het midden
     translate(width / 2, height / 2);
-    circle(0, 0, 100);
+
+    fill('lightgrey');
+    rect(- album_size / 2, - album_size / 2, album_size, album_size);
+
+    fill('black');
+    rect((album_size / 2) - ((album_size / 2) * 0.45), (album_size / 2) - ((album_size / 2) * 0.35), (album_size / 2) * 0.4, (album_size / 2) * 0.3);
+    fill('white');
+    textSize(100);
+    text('PARENTAL', 0, 0);
+
 }
