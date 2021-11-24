709d7d6057b1c820bb37f94443946e2f7f21ca50
Leander Bindt <leander.bindt@ziggo.nl>
2021-11-24 11:27:49 +0100

    Update 2.3.js

--- src/js/chapter-2/2.3.js
+++ src/js/chapter-2/2.3.js
@@ -5,6 +5,12 @@ var breedte;
 var x;
 var y;
 
+var code1;
+var code2;
+var code3;
+
+var code;
+
 function setup() {
     createCanvas(1425, 770);
     background('lightgrey');
@@ -14,16 +20,15 @@ function setup() {
 
     textSize(32);
     textAlign(CENTER);
+
+    code1 = int(random(0, 9));
+    code2 = int(random(0, 9));
+    code3 = int(random(0, 9));
+    code = [code1, code2, code3];
+    console.log(code);
 }
 
 function draw() {
-    fill(255, 255, 128, 10);
-    breedte = 3 * size + 2 * spacing;
-    x = width/2 - 0.5 * breedte;
-    y = height/2 - 0.5 * breedte;
-    ellipse(x, y - size, 10, 10);
-
-
     fill(115, 115, 115);
     stroke('darkgrey');
     breedte = 3 * size + 4 * spacing;
@@ -31,6 +36,20 @@ function draw() {
     x = width/2 - 0.5 * breedte;
     y = height/2 - 0.5 * hoogte;
     rect(x, y, breedte, hoogte + 2 * spacing, 10);
+
+    strokeWeight(3);
+    stroke(255, 230, 100);
+    fill(255, 255, 128);
+    breedte = 3 * size + 2 * spacing;
+    x = width/2;
+    y = height/2 - 0.5 * breedte;
+    ellipse(x, y - size, size - 15, size - 15);
+
+    x = width/2 - 0.3 * breedte;
+    ellipse(x, y - size, size - 15, size - 15);
+    x = width/2 + 0.3 * breedte;
+    ellipse(x, y - size, size - 15, size - 15);
+    stroke('darkgrey');
 
     fill('white');
     strokeWeight(5);
@@ -144,7 +163,9 @@ function draw() {
     fill(77, 77, 77);
 
     noStroke();
-    x = width/2 - 0.5 * breedte;
+    x = width/2 - 0.5 * breedte - 4;
     y = height/2 + 0.5 * hoogte + 1.5 * spacing;
-    rect(x, y, breedte, 40);
+    rect(x, y, breedte + 8, 40);
+
+
 }
