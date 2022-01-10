3b3359179fa269e047c5334f30123c48f1674b4c
Leander Bindt <leander.bindt@ziggo.nl>
2022-01-10 22:14:30 +0100

    Update 0.1.10
    Update Final Project Unit2
    
    Co-Authored-By: Lotte004 <90279139+Lotte004@users.noreply.github.com>

--- docs/js/chapter-2/final-project.js
+++ docs/js/chapter-2/final-project.js
@@ -8,15 +8,13 @@ let elementWidth;
 let elementHeight;
 
 const canvas =  function (p5) {
-    let r, g, b;
-    let d;
-
     let x;
     let y;
 
     let start;
 
     let buttons;
+    let pressed;
 
     let size;
 
@@ -27,23 +25,39 @@ const canvas =  function (p5) {
     let brush;
     let thickness;
     let lineColor;
+    let button;
 
     function mouseInCanvas() {
         return p5.mouseX >= 0 && p5.mouseX <= p5.width && p5.mouseY >= 0 && p5.mouseY <= p5.height;
     }
 
-    function createButton(x, y, id, color) {
-        if (buttons.length >= 12) {
-            buttons = [];
-        } else {
+    function createButton(x, y, id, color, setup) {
+        if (setup === true) {
             buttons.push([id, x, y, size, color]);
+            //console.log(buttons);
         }
 
         p5.strokeWeight(5);
         p5.stroke('#B3CDE0');
         p5.fill(color);
-        //p5.noStroke();
         p5.circle(x, y, size)
+    }
+
+    function checkButton(mouseX, mouseY) {
+        button = [];
+        for (let i = 0; i in buttons; i++) {
+            let bX = buttons[i][1];
+            let bY = buttons[i][2];
+            let bSize = buttons[i][3];
+            //console.log(p5.dist(mouseX, mouseY, bX, bY), bSize);
+            if (p5.dist(mouseX, mouseY, bX, bY) <= bSize) {
+                button.push(buttons[i][0]);
+                button.push(buttons[i][1]);
+                return button;
+            }
+        }
+        //console.log('Did not press a button'); // Console log that no button is pressed
+        return null; // Return that user did not press any button
     }
 
     p5.setup = function () {
@@ -53,6 +67,7 @@ const canvas =  function (p5) {
 
         start = true;
         buttons = [];
+        pressed = null;
 
         size = 60;
 
@@ -62,10 +77,45 @@ const canvas =  function (p5) {
         colorList = [p5.color('#e71223'), p5.color('#f6630d'), p5.color('#ffc112'),
             p5.color('#65cc02'), p5.color('#00a0d7'), p5.color('#5a2e90')];
 
-
         lineColor = p5.color(100, 151, 177);
 
         thickness = 5;
+
+        p5.noStroke();
+        p5.fill('#6497B1');
+        p5.rect(0, 0, p5.width, 1.5 * size);
+
+        xSpacing = (p5.width - 6 * size) / 7;
+        x = xSpacing + 0.5 * size;
+        y = 1.5 * size
+
+        p5.strokeWeight(5);
+        p5.stroke('#B3CDE0');
+        p5.line(0, y, p5.width, y);
+        p5.line(0, p5.height - 2.5, p5.width, p5.height - 2.5);
+
+
+        p5.rect(10, 1.5 * y, 10 + 2 * size, 3.5 * y);
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'eraser', 'red', true);
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'eraser', 'red', true);
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'eraser', 'red', true);
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'eraser', 'red', true);
+        p5.line(10, 4 * y, 20 + 2 * size, 4 * y);
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'eraser', 'red', true);
+        //p5.text();
+
+        y = y/2;
+
+        for (let i = 0; i < 6; i++) {
+            if (i <= 0) {
+                createButton(x, y, "color", colorList[i], true);
+            } else {
+                x += xSpacing + size;
+                createButton(x, y, "color", colorList[i], true);
+            }
+        }
+
+        console.log(buttons);
     }
 
     p5.draw = function () {
@@ -103,6 +153,7 @@ const canvas =  function (p5) {
                     break;
                 case 's':
                     lineColor = p5.color(p5.random(0, 255), p5.random(0, 255), p5.random(0, 255));
+                    break;
             }
         }
 
@@ -119,23 +170,26 @@ const canvas =  function (p5) {
         p5.line(0, y, p5.width, y);
         p5.line(0, p5.height - 2.5, p5.width, p5.height - 2.5);
 
+
         p5.rect(10, 1.5 * y, 10 + 2 * size, 3.5 * y);
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'brush1', 'red');
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'brush2', 'red');
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'brush3', 'red');
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'brush4', 'red');
         p5.line(10, 4 * y, 20 + 2 * size, 4 * y);
-        p5.text();
+        createButton((10 + 20 + 2 * size) / 2, (4 * y + 5 * y) / 2, 'eraser', 'red');
+        //p5.text();
 
         y = y/2;
 
         for (let i = 0; i < 6; i++) {
             if (i <= 0) {
-                createButton(x, y, "Yellow", colorList[i]);
+                createButton(x, y, "color", colorList[i]);
             } else {
                 x += xSpacing + size;
-                createButton(x, y, "Yellow", colorList[i]);
+                createButton(x, y, "color", colorList[i]);
             }
         }
-
-        p5.stroke(r, g, b);
-        p5.fill(r, g, b);
 
         d = p5.dist(p5.mouseX, p5.mouseY, x, y);
 
@@ -155,6 +209,40 @@ const canvas =  function (p5) {
                         break;
                 }
             }
+        }
+    }
+
+    p5.mouseClicked = function () {
+        if (!mouseInCanvas()) {
+            return;
+        }
+        pressed = checkButton(p5.mouseX, p5.mouseY);
+        console.log(pressed);
+        if (pressed === null) {
+            return;
+        } else if (pressed[0] === 'color') {
+            switch (pressed[1]) {
+                case buttons[5][1]:
+                    lineColor = colorList[0];
+                    break;
+                case buttons[6][1]:
+                    lineColor = colorList[1];
+                    break;
+                case buttons[7][1]:
+                    lineColor = colorList[2];
+                    break;
+                case buttons[8][1]:
+                    lineColor = colorList[3];
+                    break;
+                case buttons[9][1]:
+                    lineColor = colorList[4];
+                    break;
+                case buttons[10][1]:
+                    lineColor = colorList[5];
+                    break;
+            }
+        } else if (pressed[0] === 'eraser') {
+            lineColor = p5.color('lightgrey');
         }
     }
 }
