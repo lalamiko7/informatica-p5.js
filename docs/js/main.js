93c5e9a900ed0bf08a5be7343745712e7d1eb38c
Leander Bindt <leander.bindt@ziggo.nl>
2022-01-15 17:31:07 +0100

    Update 0.1.20
    Navigation now works on mobile. Will improve design later.

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,7 +2,7 @@ let nav_name_count = 0;
 let nav_links_count = 0;
 let nav_menu_symbool_count = 0;
 
-function nav_menu_animatie() {
+function symbol() {
     const nav_menu_symbool = document.getElementById("nav-menu-symbool");
     if (nav_menu_symbool_count === 0) {
         nav_menu_symbool.classList.toggle("nav-menu-symbool-animatie-open");
@@ -16,30 +16,50 @@ function nav_menu_animatie() {
         nav_menu_symbool.classList.toggle("nav-menu-symbool-animatie-open");
         nav_menu_symbool_count -= 1;
     }
+}
 
-    const nav_name = document.getElementById("nav-name");
-    if (nav_name_count === 0) {
-        nav_name.classList.toggle("nav-name-animatie-out");
-        nav_name_count += 1;
-    } else if (nav_name_count === 1) {
-        nav_name.classList.toggle("nav-name-animatie-out");
-        nav_name.classList.toggle("nav-name-animatie-in");
-        nav_name_count += 1;
-    } else if (nav_name_count === 2) {
-        nav_name.classList.toggle("nav-name-animatie-in");
-        nav_name.classList.toggle("nav-name-animatie-out");
-        nav_name_count -= 1;
-    }
 
-    const nav_links = document.getElementById("nav-links");
-    if (nav_links_count === 0) {
-        nav_links.style.right = "0";
-        nav_links_count += 1;
-    } else if (nav_links_count === 1) {
-        nav_links.style.right = "-100%";
-        nav_links_count += 1;
-    } else if (nav_links_count === 2) {
-        nav_links.style.right = "0";
-        nav_links_count -= 1;
+function nav_menu_animatie() {
+    symbol();
+
+    if (window.innerWidth >= 1000) {
+        const nav_name = document.getElementById("nav-name");
+        if (nav_name_count === 0) {
+            nav_name.classList.toggle("nav-name-animatie-out");
+            nav_name_count += 1;
+        } else if (nav_name_count === 1) {
+            nav_name.classList.toggle("nav-name-animatie-out");
+            nav_name.classList.toggle("nav-name-animatie-in");
+            nav_name_count += 1;
+        } else if (nav_name_count === 2) {
+            nav_name.classList.toggle("nav-name-animatie-in");
+            nav_name.classList.toggle("nav-name-animatie-out");
+            nav_name_count -= 1;
+        }
+
+        const nav_links = document.getElementById("nav-links");
+        if (nav_links_count === 0) {
+            nav_links.style.right = "0";
+            nav_links_count += 1;
+        } else if (nav_links_count === 1) {
+            nav_links.style.right = "-100%";
+            nav_links_count += 1;
+        } else if (nav_links_count === 2) {
+            nav_links.style.right = "0";
+            nav_links_count -= 1;
+        }
+    } else {
+        const nav_links = document.getElementById("nav-links");
+        const height = nav_links.clientHeight;
+        if (nav_links_count === 0) {
+            nav_links.style.top = "1vh";
+            nav_links_count += 1;
+        } else if (nav_links_count === 1) {
+            nav_links.style.top = "-10vh";
+            nav_links_count += 1;
+        } else if (nav_links_count === 2) {
+            nav_links.style.top = "1vh";
+            nav_links_count -= 1;
+        }
     }
 }
